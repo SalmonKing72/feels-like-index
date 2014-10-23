8a34837ec7891cca3f58a354bc35713051ca8383
Joey Madich <joeymadich@Joeys-MacBook-Pro.local>
2014-10-22 22:30:27 -0400

    updating mockweather object file and calculator source file for JSLint validation

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,20 +1,24 @@
 function Calculator() {
+    "use strict";
 }
-Calculator.prototype.currentWeather = function(obj){
-  if(obj !== null && obj !== undefined) {
-    this.description = obj.weather[0].description;
-    this.currentTempFahr = Math.ceil((obj.main.temp - 273.15) * 1.80 + 32);
-    this.currentTempCels = obj.main.temp - 273.15;
-  }
-  else {
-    throw new Error("the current weather is unknown at this time.");
-  }
+Calculator.prototype.currentWeather = function (obj) {
+    "use strict";
+    if (obj !== null && obj !== undefined) {
+        this.description = obj.weather[0].description;
+        this.currentTempFahr = Math.ceil((obj.main.temp - 273.15) * 1.80 + 32);
+        this.currentTempCels = obj.main.temp - 273.15;
+    } else {
+        throw new Error("the current weather is unknown at this time.");
+    }
 };
 
-Calculator.prototype.currentWindChill = function(obj){
-  if(obj !== null && obj !== undefined) {
-    var windSpeed = obj.wind.speed;
-    var temperature = this.currentTempFahr;
-    var WIND_CHILL_CONSTANTS = [35.74, 0.6215, 35.75, 0.4275, 0.16];
-    //if(windSpeed)
-}
+Calculator.prototype.calculateWindChill = function (obj) {
+    "use strict";
+    if (obj !== null && obj !== undefined) {
+        var windSpeed = obj.wind.speed,
+            temperature = this.currentTempFahr,
+            WIND_CHILL_CONSTANTS = [35.74, 0.6215, 35.75, 0.4275, 0.16];
+        //if(windSpeed is fast enough and temp. is cold enough) calculate wind chill
+        //if(humidity is high enough and temp. is warm enough) calculate heat index.
+    }
+};
--- src/MockWeather.js
+++ src/MockWeather.js
@@ -1,17 +1,18 @@
 function MockWeather() {
-  var weatherObj = {
-    "coord":{"lon":-83,"lat":39.96},
-    "sys":{"type":1,"id":2191,"message":0.1932,"country":"United States of America","sunrise":1411644186,"sunset":1411687427},
-    "weather":[{"id":800,"main":"Clear","description":"sky is clear","icon":"01d"}],
-    "base":"cmc stations",
-    "main":{"temp":297.51,"pressure":1023,"humidity":38,"temp_min":296.15,"temp_max":299.15},
-    "wind":{"speed":2.6,"deg":60},
-    "clouds":{"all":1},
-    "dt":1411685220,
-    "id":4509177,
-    "name":"Columbus",
-    "cod":200
-  };
+    "use strict";
+    var weatherObj = {
+        "coord" : {"lon" : -83, "lat" : 39.96},
+        "sys" : {"type" : 1, "id" : 2191, "message" : 0.1932, "country" : "United States America", "sunrise" : 1411644186, "sunset" : 1411687427},
+        "weather" : [{"id" : 800, "main" : "Clear", "description" : "sky is clear", "icon" : "01d"}],
+        "base" : "cmc stations",
+        "main" : {"temp" : 297.51, "pressure" : 1023, "humidity" : 38, "temp_min" : 296.15, "temp_max" : 299.15},
+        "wind" : {"speed" : 2.6, "deg" : 60},
+        "clouds" : {"all" : 1},
+        "dt" : 1411685220,
+        "id" : 4509177,
+        "name" : "Columbus",
+        "cod" : 200
+    };
 
-  return weatherObj;
+    return weatherObj;
 }
